16ef64bf920645318decba9bc386d3563be82d8f
Garrett <garrettodom97@gmail.com>
2021-06-16 11:35:25 -0500

    Add's controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,11 @@
+class ContactsController < ApplicationController
+  def first_contact
+    contact = Contact.first
+    render json: contact.as_json
+  end
+
+  def all_contacts
+    contacts = Contact.all
+    render json: contacts.as_json
+  end
+end
